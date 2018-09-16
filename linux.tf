b64f15b0bf36fc6b5582383729c762f54deda7c6
Gordon Murray <gordon@gordonmurray.com>
2018-09-16 12:51:21 +0100

    A windows and a Linux VM

---
+++ linux.tf
@@ -0,0 +1,57 @@
+variable "linux_prefix" {
+  default = "linux"
+}
+
+data "local_file" "pem" {
+  filename = "/home/gordon/.ssh/id_rsa.pub"
+}
+
+resource "azurerm_network_interface" "linux" {
+  name                = "${var.linux_prefix}-nic"
+  location            = "${module.resource_group.resource_group_location}"
+  resource_group_name = "${module.resource_group.resource_group_name}"
+
+  ip_configuration {
+    name                          = "${var.linux_prefix}-ip-configuration"
+    subnet_id                     = "${module.subnet.subnet_id}"
+    private_ip_address_allocation = "dynamic"
+    public_ip_address_id          = "${module.public_ip_linux.public_ip_id}"
+  }
+}
+
+resource "azurerm_virtual_machine" "linux" {
+  name                  = "${var.linux_prefix}-vm"
+  location              = "${module.resource_group.resource_group_location}"
+  resource_group_name   = "${module.resource_group.resource_group_name}"
+  network_interface_ids = ["${azurerm_network_interface.linux.id}"]
+  vm_size               = "Standard_DS1_v2"
+
+  storage_image_reference {
+    publisher = "Canonical"
+    offer     = "UbuntuServer"
+    sku       = "18.04-LTS"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name              = "linux_disk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name  = "Ubuntu"
+    admin_username = "ubuntu"
+    admin_password = "Password12345!"
+  }
+
+  os_profile_linux_config {
+    disable_password_authentication = true
+
+    ssh_keys {
+      path     = "/home/ubuntu/.ssh/authorized_keys"
+      key_data = "${data.local_file.pem.content}"
+    }
+  }
+}
---
+++ windows.tf
@@ -0,0 +1,48 @@
+variable "prefix" {
+  default = "windows"
+}
+
+resource "azurerm_network_interface" "main" {
+  name                = "${var.prefix}-nic"
+  location            = "${module.resource_group.resource_group_location}"
+  resource_group_name = "${module.resource_group.resource_group_name}"
+
+  ip_configuration {
+    name                          = "${var.prefix}-ip-configuration"
+    subnet_id                     = "${module.subnet.subnet_id}"
+    private_ip_address_allocation = "dynamic"
+    public_ip_address_id          = "${module.public_ip_windows.public_ip_id}"
+  }
+}
+
+resource "azurerm_virtual_machine" "windows" {
+  name                  = "${var.prefix}-vm"
+  location              = "${module.resource_group.resource_group_location}"
+  resource_group_name   = "${module.resource_group.resource_group_name}"
+  network_interface_ids = ["${azurerm_network_interface.main.id}"]
+  vm_size               = "Standard_DS1_v2"
+
+  storage_image_reference {
+    publisher = "MicrosoftWindowsServer"
+    offer     = "WindowsServer"
+    sku       = "2008-R2-SP1"
+    version   = "latest"
+  }
+
+  storage_os_disk {
+    name              = "windows_disk"
+    caching           = "ReadWrite"
+    create_option     = "FromImage"
+    managed_disk_type = "Standard_LRS"
+  }
+
+  os_profile {
+    computer_name  = "Windows"
+    admin_username = "AdminPerson"
+    admin_password = "Password12345!"
+  }
+
+  os_profile_windows_config {
+    enable_automatic_upgrades = false
+  }
+}
